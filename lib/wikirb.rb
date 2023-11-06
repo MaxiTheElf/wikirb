f374d2095f11118ff6c9052b3c4d4285ad785ac1
Maxi <voynich@disroot.org>
2023-11-06 17:17:44 -0500

    add search method

--- lib/wikirb.rb
+++ lib/wikirb.rb
@@ -1,6 +1,14 @@
+require 'net/http'
+
 class WikiRB
     # a cute greeting to test wikirb
     def self.haiii
         puts 'Haiii, World!'
+    end
+
+    # search wikipedia
+    def self.search(query, limit, namespace)
+        uri = URI("https://en.wikipedia.org/w/api.php?action=opensearch&search=#{query}&limit=#{limit}&namespace=#{namespace}")
+        Net::HTTP.get(uri)
     end
 end
